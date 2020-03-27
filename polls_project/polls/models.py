023deba798edeaae6dd0db8d7aa089dad91d4dd9
austen cote <coteausten@gmail.com>
2020-03-27 16:23:55 -0700

    first commit created db and started views

---
+++ polls_project/polls/models.py
@@ -0,0 +1,27 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+#create a class for question that stores the question and the pub date 
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+#create a class for a answer choice and put a answer char field and a votes with int filed 
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
