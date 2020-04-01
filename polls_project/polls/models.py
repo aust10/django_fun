f87080c702e3664a36a1a2efa416fe296154129e
austen cote <coteausten@gmail.com>
2020-04-01 12:47:37 -0700

    created a new polls project

--- polls_project/polls/models.py
+++ polls_project/polls/models.py
@@ -13,10 +13,10 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 #create a class for a answer choice and put a answer char field and a votes with int filed 
-class Choices(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default=0)
